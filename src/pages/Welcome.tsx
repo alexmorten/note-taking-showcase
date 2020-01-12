952d4f61d7ef6c48389b41cb9beb8b31b70e62b9
alexmorten <alexandrius.martinus@gmail.com>
2020-01-12 14:58:37 +0100

    redesign welcome page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,45 +1,55 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "./Welcome.scss";
 export default function Welcome() {
   return (
-    <>
-      <h1>What is this?</h1>
-      <p>
-        This is a simple, light-weight{" "}
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://en.wikipedia.org/wiki/Markdown"
-        >
-          markdown
-        </a>{" "}
-        note taking app.
-      </p>
-      <p>
-        It stores everything locally in your browser and you can still use it
-        without an internet connection.
-      </p>
-
-      <h2>Why?</h2>
-      <p>This is mostly an excercise for myself.</p>
-
-      <h2>How?</h2>
-      <p>
-        Go to <Link to="/notes">your notes</Link>{" "}
-      </p>
-
-      <h2>Show me the code!</h2>
-      <p>
-        Just have a look at the{" "}
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/alexmorten/note-taking-showcase"
-        >
-          repo
-        </a>
-      </p>
-    </>
+    <div className="Welcome">
+      <section className="section1">
+        <h1>What is this?</h1>
+        <p>
+          This is a simple, light-weight{" "}
+          <a
+            className="inherit-color"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://en.wikipedia.org/wiki/Markdown"
+          >
+            markdown
+          </a>{" "}
+          note taking app.
+        </p>
+        <p>
+          It stores everything locally in your browser and you can still use it
+          without an internet connection.
+        </p>
+      </section>
+      <section className="section2">
+        <h2>Why?</h2>
+        <p>This is mostly an excercise for myself.</p>
+      </section>
+      <section className="section3">
+        <h2>How?</h2>
+        <p>
+          Go to{" "}
+          <Link className="inherit-color" to="/notes">
+            your notes
+          </Link>{" "}
+        </p>
+      </section>
+      <section className="section4">
+        <h2>Show me the code!</h2>
+        <p>
+          Just have a look at the{" "}
+          <a
+            className="inherit-color"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/alexmorten/note-taking-showcase"
+          >
+            repo
+          </a>
+        </p>
+      </section>
+    </div>
   );
 }
